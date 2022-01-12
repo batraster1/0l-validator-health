779615943d3c0eae2c4b508c53d688cd6c282d4c
batraster1 <36015640+batraster1@users.noreply.github.com>
2022-01-12 09:56:24 -0700

    added prod webhook and masking

--- discordNotifier.js
+++ discordNotifier.js
@@ -1,16 +1,17 @@
 const { Webhook, MessageBuilder } = require('discord-webhook-node');
 
 const WEBHOOK_NAME = "ValidatorHealth"
-const DEFAULT_WEBHOOK = "https://discord.com/api/webhooks/927400770557575279/F2alVR5ZqVTCir-31SCRVcPbEOKuHlKulqRyvem-MuZwCO9xxM8IhvazmO8pXISS6Mzs"
-const TEST_WEBHOOK = "https://discord.com/api/webhooks/927400691201372170/bY_U-pUMESzl0MHJ3fXisVU1gbYYrvZI1cwiAGL9yMuObKzsoevGYbv8gAZ5D8KoeSsm"
+const DEFAULT_WEBHOOK = "https://discord.com/api/webhooks/928379458400972821/5r3YseN-HhYKa797aOWtg8s5_BW6iZVCkY82l6Z0sU0bZC4S5C-d6m-S3NQhatJaSkJX"
+const TEST_WEBHOOK = "https://discord.com/api/webhooks/929581478222901329/Q_dtP9v0t-dwyzpNdbFDQbLCrrSjSbzXN9Y6LLV2Ls_aqWBq9z0nRInBh6El9Z4meiHB"
+const PROD_WEBHOOK = "https://discord.com/api/webhooks/929582767828795463/WY_qk-_IAJ7oiFxtWqCXTnN_3f7sohkL5MBsj_xETNnlHAnIirtMHNnoahocVGPLyLU_"
 
 function sendToWebhooks(validator, fields, color){
     if(validator.webhookUrls && validator.webhookUrls.length >0){
         const embed = new MessageBuilder().setColor(color)
 
-        let footer = `IP: ${validator.ipAddress}`   
+        let footer = `IP: ${maskCharacter(validator.ipAddress)}`   
         if(validator.address){
-            footer += `\nAddress: ${validator.address}`
+            footer += `\nAddress: ${maskCharacter(validator.address)}`
         }
         for(let webhookUrl of validator.webhookUrls){
             if(webhookUrl === "DEFAULT"){
@@ -18,6 +19,9 @@ function sendToWebhooks(validator, fields, color){
             }
             if(webhookUrl == "TEST"){
                 webhookUrl = TEST_WEBHOOK
+            }
+            if(webhookUrl == "PROD"){
+                webhookUrl = PROD_WEBHOOK
             }
 
             if(fields && fields.length > 0){
@@ -35,9 +39,21 @@ function sendToWebhooks(validator, fields, color){
             embed.setFooter(footer)
             const hook = new Webhook(webhookUrl)
             hook.setUsername(WEBHOOK_NAME)
+            
             hook.send(embed)
+            .then(() => console.log('Sent webhook successfully!'))
+            .catch(err => console.log(`Error sending to webhook: ${validator.description} ${webhookUrl} \n ${err}`))            
         }
     }
+}
+
+function maskCharacter(str, mask = '#', n = 4) {
+  
+    // Slice the string and replace with
+    // mask then add remaining string
+    return ('' + str).slice(0, -n)
+        .replace(/./g, mask)
+        + ('' + str).slice(-n);
 }
 
 module.exports = {sendToWebhooks}
