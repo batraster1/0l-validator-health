714817067a957cf1ca754081f215f2b56ac5256b
Dung Ngo <dung.ngo@eastagile.com>
2023-01-18 11:24:00 +0700

    Add notification via email using Sendgrid

--- README.md
+++ README.md
@@ -2,25 +2,23 @@
 
 A Node project that tracks the health of 0L validator nodes/Sentry project error alerts and publishes notifications to Slack.
 
-
-  
-
 ## Health Checks
 
- 1. Node Offline
- 2. Not in Sync
- 3. Not in validator set
- 4. Not running
- 5. Votes in epoch unchanged in the last 5 min
- 6. Proofs in epoch unchanged - If the proofs submitted by a validator is less than 8 and no new proofs have been submitted in the last 3 hours.
- 
-![11d9f8d52f78.png\]](https://user-images.githubusercontent.com/36015640/148246687-a2193d44-da74-47de-aa74-11d9f8d52f78.png)
+1.  Node Offline
+2.  Not in Sync
+3.  Not in validator set
+4.  Not running
+5.  Votes in epoch unchanged in the last 5 min
+6.  Proofs in epoch unchanged - If the proofs submitted by a validator is less than 8 and no new proofs have been submitted in the last 3 hours.
+
+![11d9f8d52f78.png]](https://user-images.githubusercontent.com/36015640/148246687-a2193d44-da74-47de-aa74-11d9f8d52f78.png)
 
 ## Adding monitoring for your validator node
+
 To get monitoring add a new file called [credential.toml](./credential.toml)
 
     # CONFIG NEEDED FOR SENTRY ALERT
-	PORT = 8000
+    PORT = 8000
     DEFAULT_WEBHOOK = "https://hooks.slack.com/services/.../.../"
 
     # CONFIG FOR 0L VALIDATOR HEALTH
@@ -29,20 +27,25 @@ To get monitoring add a new file called [credential.toml](./credential.toml)
     VALIDATORS = ["xxx.xxx.xxx.xxx"]
     ERROR_TYPES = ["ALL"]
     INTERVAL = 36000 # 1 hour
+    SENDGRID_API_KEY = ""
+    SENDGRID_EMAIL_FROM = ""
+    SENDGRID_EMAIL_TO = ""
+    SENDGRID_EMAIL_CC = ""
     
- - WEBHOOKS: Slack webhooks.
- - FULL_NODE_IP: IP of a 0L network full node   
- - VALIDATORS : IP addresses of your validator nodes. Ensure the web monitor is running and port 3030 is open
- - Special case for Sentry you need to set up a new internal integration, set new alert rules to send webhook, set Alert Callback URLs to this server. Default is [localhost:8000/sentry_call_back](localhost:8000/sentry_call_back)
+- WEBHOOKS: Slack webhooks.
+- FULL_NODE_IP: IP of a 0L network full node
+- VALIDATORS: IP addresses of your validator nodes. Ensure the web monitor is running and port 3030 is open
+- Special case for Sentry you need to set up a new internal integration, set new alert rules to send webhook, set Alert Callback URLs to this server. Default is [localhost:8000/sentry_call_back](localhost:8000/sentry_call_back)
+- SENDGRID_*: For sending notification via email. SENDGRID_EMAIL_FROM is associated with SENDGRID_API_KEY.
 
 ### How to use
+
 If you'd like to run the monitor yourself:
 
     clone repo
     npm install
     add credential.toml
-    # FOR SENTRY ALERT 
+    # FOR SENTRY ALERT
     node index.js
     # FOR 0L HEALTH
     node validator.js
-
--- validator.js
+++ validator.js
@@ -1,26 +1,31 @@
-const {IncomingWebhook} = require("@slack/webhook");
-const  eventSource = require('eventsource');
-const errorChecker = require("./errorChecker.js")
+const { IncomingWebhook } = require("@slack/webhook");
+const eventSource = require('eventsource');
 const toml = require('toml');
+const fs = require('fs');
+const path = require('path');
+const sgMail = require('@sendgrid/mail')
+
+const errorChecker = require("./errorChecker.js")
+
 const CONFIG_ENV = process.env
 const CONFIG_TOML = 'credential.toml'
-const fs = require('fs');
 const validatorErrorMap = new Map()
 
 let parsed = [];
 let CONFIG = [];
-if(!fs.existsSync(CONFIG_TOML)) {
+if (!fs.existsSync(CONFIG_TOML)) {
     console.log("File not found, skip config file");
     CONFIG["VALIDATORS"] = CONFIG_ENV.VALIDATORS_DATA.split(" ");
     CONFIG["ERROR_TYPES"] = CONFIG_ENV.ERROR_TYPES_DATA.split(" ");
-    CONFIG = {...CONFIG, ...CONFIG_ENV}
+    CONFIG = { ...CONFIG, ...CONFIG_ENV }
 } else {
     parsed = toml.parse(fs.readFileSync(CONFIG_TOML, 'utf8'));
-    CONFIG = { ...parsed}
+    CONFIG = { ...parsed }
 }
+sgMail.setApiKey(CONFIG.SENDGRID_API_KEY)
 
 function getValidatorStats() {
-    for(let validator of CONFIG.VALIDATORS){
+    for (let validator of CONFIG.VALIDATORS) {
         let uri = `http://${validator}:3030/vitals`
 
         try {
@@ -31,39 +36,40 @@ function getValidatorStats() {
                 let errors = await errorChecker.checkForErrors(validator, CONFIG, response)
                 //validator = response.account_view.address
                 calNotification(validator, errors)
+                if (errors.length > 0) {
+                    sendEmail(errors)
+                } else {
+                    console.log("Validator is functional")
+                }
             }
             sse.onerror = (err) => {
                 sse.close()
-                calNotification(validator,['OFFLINE'])
+                calNotification(validator, ['OFFLINE'])
+                console.log(`Error from validator: ${err}`)
             }
         } catch (err) {
-            console.log('error')
-            calNotification(validator,['OFFLINE'])
+            calNotification(validator, ['OFFLINE'])
+            console.log(`Error from eventSource: ${err}`)
         }
     }
-
 }
 
-console.log('Starting 0l validator health ...')
-//getValidatorStats()
-setInterval(getValidatorStats, CONFIG.INTERVAL);
-
-function calNotification(validator, newErrors){
+function calNotification(validator, newErrors) {
     let errorKey = validator
     //console.log(newErrors)
     const hasErrors = validatorErrorMap.has(errorKey);
-    if( newErrors.length === 0 && hasErrors){
+    if (newErrors.length === 0 && hasErrors) {
         sendNotification(validator, [])
         validatorErrorMap.delete(errorKey)
     }
-    else if(newErrors.length > 0 && !hasErrors ){
-        sendNotification(validator, {value: newErrors.join("\n")})
+    else if (newErrors.length > 0 && !hasErrors) {
+        sendNotification(validator, { value: newErrors.join("\n") })
         validatorErrorMap.set(errorKey, newErrors)
     }
-    else if(newErrors.length > 0 && hasErrors){
+    else if (newErrors.length > 0 && hasErrors) {
         let errorsCleared = validatorErrorMap.get(errorKey).filter(error => !newErrors.includes(error))
-        if(errorsCleared.length >0){
-            sendNotification(validator, {value: newErrors.join("\n")})
+        if (errorsCleared.length > 0) {
+            sendNotification(validator, { value: newErrors.join("\n") })
         }
         validatorErrorMap.set(errorKey, newErrors)
     }
@@ -111,3 +117,29 @@ function sendNotification(ip, err) {
     })();
 }
 
+function sendEmail(err) {
+    let email_content = fs.readFileSync(path.join(__dirname, "email/validator_error.txt"))
+    let html_email_content = fs.readFileSync(path.join(__dirname, "email/validator_error.html"))
+
+    const message = {
+        to: CONFIG.SENDGRID_EMAIL_TO,
+        cc: CONFIG.SENDGRID_EMAIL_CC,
+        from: CONFIG.SENDGRID_EMAIL_FROM,
+        subject: '[0L Validator Health] New error message',
+        text: eval("`" + email_content + "`"),
+        html: eval("`" + html_email_content + "`"),
+    }
+
+    console.log("Sending email")
+    sgMail
+        .send(message)
+        .then((response) => {
+            console.log(`Email sent with response code ${response[0].statusCode}`)
+        })
+        .catch((error) => {
+            console.error(error)
+        })
+}
+
+console.log('Starting 0l validator health ...')
+setInterval(getValidatorStats, CONFIG.INTERVAL);
